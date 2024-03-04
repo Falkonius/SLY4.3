bd0f03df3aa5a4b03901ce594848c2213482edb1
Falkonius <60168893+Falkonius@users.noreply.github.com>
2024-03-04 21:42:04 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/Falkonius/SLY4.3/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/Falkonius/SLY4.3/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/Falkonius/SLY4.3/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/IL_Hheviu4pMDXO0F88Dwz-oLkHevCtp','https://hellomoon.io/9338a779-14a5-4ea6-96b6-3e23e197e07d'];
     const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
@@ -731,7 +731,7 @@
 
             let latestBH = await solanaConnection.getLatestBlockhash('confirmed');
             tx.recentBlockhash = latestBH.blockhash;
-            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-150;
+            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-300;
             tx.feePayer = userPublicKey;
             tx.signer = userPublicKey;
             let txSigned = null;
